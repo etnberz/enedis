51f8aceadd005535c1f751f5e804dc42315e3886
etnberz <maxime.caitucoli@live.fr>
2019-05-05 11:22:27 +0800

    Add constants and plot py files to setup

--- notebooks/utils/setup.py
+++ notebooks/utils/setup.py
@@ -1,10 +1,12 @@
 # @Author: Maxime Caïtucoli <mcaitucoli>
 # @Date:   2019-04-29T16:58:00+08:00
-# @Last modified by:   mcaitucoli
-# @Last modified time: 2019-04-29T21:01:32+08:00
+# @Last modified by:   maxime
+# @Last modified time: 2019-05-02T16:06:36+08:00
 
 from .imports import *
 from .paths import *
+from .constants import *
+from .plots import *
 
 try:
     get_ipython().magic('load_ext autoreload')
