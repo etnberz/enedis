55e94fe2c2ecba0e6df91c3bc00886bf72bc51e8
etnberz <maxime.caitucoli@live.fr>
2019-05-05 11:20:50 +0800

    Had dateutil import

--- notebooks/utils/imports.py
+++ notebooks/utils/imports.py
@@ -1,9 +1,11 @@
 # @Author: Maxime Caïtucoli <mcaitucoli>
 # @Date:   2019-04-29T19:58:31+08:00
-# @Last modified by:   mcaitucoli
-# @Last modified time: 2019-04-29T20:36:23+08:00
+# @Last modified by:   maxime
+# @Last modified time: 2019-05-02T16:06:39+08:00
 
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from dateutil.parser import parse as date_parser
+import matplotlib.gridspec as gridspec
