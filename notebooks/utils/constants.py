c9e9b93799c5ad447fd35cee8f24e4d442a4edb0
etnberz <maxime.caitucoli@live.fr>
2019-05-05 11:20:25 +0800

    Create a constants py file

---
+++ notebooks/utils/constants.py
@@ -0,0 +1,7 @@
+# @Author: Maxime Caïtucoli <maxime>
+# @Date:   2019-05-01T15:08:44+08:00
+# @Last modified by:   maxime
+# @Last modified time: 2019-05-02T15:50:57+08:00
+
+TARGETS = ['RES1_BASE', 'RES11_BASE', 'RES2_HC', 'RES2_HP',
+           'PRO1_BASE', 'PRO2_HC', 'PRO2_HP']
